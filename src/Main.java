75a07dcc2ea3bec8e51a73e06db569748c7e6496
Akali29 <mahad.hussein@stu.ocsb.ca>
2024-09-17 14:37:11 -0400

    I did work

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		/*
 		 * MadLibs Program
-		 * Author: 
+		 * Author: Mahad
 		 * Class: ICS3U
 		 * Date: Sept. 16, 2024
 		 * Make a program that asks for 10 inputs
@@ -16,8 +16,23 @@ public class Main {
 		System.out.println("Welcome to Mad Libs!");
 		System.out.println("What's your name?");
 		String name = in.nextLine();
-		System.out.println("One day, "+name+" wrote a Mad Lib in class!");
-
+		System.out.println("Give me a number");
+		System.out.println("One day, "+name+ " wrote a Mad Lib in class!");
+		System.out.println("Give me a sport");
+		int num1 = in.nextInt();
+		
+		System.out.println("Give me a place");
+		//flush
+		String dayOfWeek = in.nextLine();
+		String person1 = in.nextLine();
+		String person2 = in.nextLine();
+		String sport = in.nextLine();
+		in.nextLine();
+		String place = in.nextLine();
+		System.out.println("On "+dayOfWeek+","+person1+" and "+person2+" were playing "+sport+" when a seagull crashed the game and scored 15 points");
+				
+				
+				
 	}
 
 }
